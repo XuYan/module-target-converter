2d80db2dc2a2461b6a32d672f189ba70f6442a25
XuYan <xuwithwill@gmail.com>
2020-07-05 11:51:23 -0700

    Change to convert between module and bazel target.

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let from_import = document.getElementById('import')
 let import_output = document.getElementById('import_output')
 from_import.addEventListener('change', event => {
-    import_output.value = from_import.value.replace(/_/g, '/')
+    import_output.value = '//' + from_import.value.replace(/_/g, '/')
     navigator.clipboard.writeText(import_output.value).then(
         () => {},
         () => { console.err("failed to copy to clipboard") }
@@ -12,7 +12,11 @@ from_import.addEventListener('change', event => {
 let from_path = document.getElementById('path')
 let path_output = document.getElementById('path_output')
 from_path.addEventListener('change', event => {
-    path_output.value = from_path.value.replace(/\//g, '_')
+    var target = from_path.value
+    if (target.startsWith('//')) {
+        target = target.substr(2)
+    }
+    path_output.value = target.replace(/\//g, '_')
     navigator.clipboard.writeText(path_output.value).then(
         () => {},
         () => { console.err("failed to copy to clipboard") }
